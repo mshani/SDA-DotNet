1900cac9264bf02540fb862572e14f681be5f213
Marsela Shani <marsela.shani@gmail.com>
2023-06-17 14:30:06 +0200

    Adv11

---
+++ Adv10/CarService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv10
+{
+    internal class CarService
+    {
+        public List<Car> Cars { get; private set; }
+        public CarService() { 
+            Cars = new List<Car>();
+        }
+        public void AddCar(Car car)
+        {          
+            if (car != null){
+                bool exists = false;
+                //foreach (var element in Cars){
+                //    if (element.Equals(car)){
+                //        exists = true; 
+                //        break;
+                //    }
+                //}
+                exists = Cars.Any(element => element.Equals(car));
+                if(!exists)
+                    Cars.Add(car);
+            }
+        }
+        public void RemoveCar(Car car)
+        {
+            if (car != null)
+            {
+                bool exists = false;
+                //foreach (var element in Cars)
+                //{
+                //    if (element.Equals(car))
+                //    {
+                //        exists = true;
+                //        break;
+                //    }
+                //}
+                exists = Cars.Any(element => element.Equals(car));
+                if (exists)
+                    Cars.Remove(car);
+            }
+        }
+        public void ListAll(List<Car> cars = null)
+        {
+            var toList = cars;
+            if (cars == null){
+                toList = Cars;
+            }
+
+            foreach (var car in toList)
+            {
+                Console.WriteLine($"{car.Name} {car.Model} {car.Year} {car.Price}");
+            }
+        }
+        public List<Car> FindCarsByEngineType(EngineTypeEnum engineType)
+        {
+            var result = Cars
+                .Where(x => x.EngineType == engineType)
+                .ToList();
+            return result;
+        }
+        public List<Car> FindCarsBeforeYear(int year)
+        {
+            var result = Cars
+                        .Where(x => x.Year < year)
+                        .ToList();
+            return result;
+        }
+        public Car FindTheMostExpensiveCar()
+        {
+            var price = Cars.Max(x => x.Price);
+            var result = Cars
+                .Where(x => x.Price == price)
+                .FirstOrDefault();
+            return result;
+        }
+        public Car FindTheLeastExpensiveCar()
+        {
+            var price = Cars.Min(x => x.Price);
+            var result = Cars
+                .Where(x => x.Price == price)
+                .FirstOrDefault();
+            return result;
+        }
+
+    }
+}
--- Adv10/Program.cs
+++ Adv10/Program.cs
@@ -1,37 +1,44 @@
-﻿using Adv10;
+﻿//using Adv10;
 
-Manufacturer toyota1 = new Manufacturer()
-{
-    Country = "Japan",
-    Year = 1950,
-    Name = "Toyota"
-};
+//Manufacturer toyota1 = new Manufacturer()
+//{
+//    Country = "Japan",
+//    Year = 1950,
+//    Name = "Toyota"
+//};
 
-Manufacturer toyota2 = toyota1;
+//Manufacturer toyota2 = toyota1;
 
-Manufacturer bmw1 = new Manufacturer()
-{
-    Country = "germany",
-    Year = 1961,
-    Name = "BMW"
-};
+//Manufacturer bmw1 = new Manufacturer()
+//{
+//    Country = "germany",
+//    Year = 1961,
+//    Name = "BMW"
+//};
 
-Manufacturer bmw2 = new Manufacturer()
-{
-    Country = "Germany",
-    Year = 1961,
-    Name = "BMW"
-};
+//Manufacturer bmw2 = new Manufacturer()
+//{
+//    Country = "Germany",
+//    Year = 1961,
+//    Name = "BMW"
+//};
 
-Console.WriteLine($"comparing toyota1 againts null: {toyota1.Equals(null)}");
+//Console.WriteLine($"comparing toyota1 againts null: {toyota1.Equals(null)}");
 
-Console.WriteLine($"comparing toyota1 againts toyota2: {toyota1.Equals(toyota2)}");
+//Console.WriteLine($"comparing toyota1 againts toyota2: {toyota1.Equals(toyota2)}");
 
-Console.WriteLine($"comparing toyota1 againts bmw1: {toyota1.Equals(bmw1)}");
+//Console.WriteLine($"comparing toyota1 againts bmw1: {toyota1.Equals(bmw1)}");
 
-Console.WriteLine($"comparing bmw1 againts bmw2: {bmw1.Equals(bmw2)}");
+//Console.WriteLine($"comparing bmw1 againts bmw2: {bmw1.Equals(bmw2)}");
 
-Console.WriteLine($"Toyota1 {toyota1.GetHashCode()}");
-Console.WriteLine($"Toyota2 {toyota2.GetHashCode()}");
-Console.WriteLine($"BMW1 {bmw1.GetHashCode()}");
-Console.WriteLine($"BMW2 {bmw2.GetHashCode()}");
+//Console.WriteLine($"Toyota1 {toyota1.GetHashCode()}");
+//Console.WriteLine($"Toyota2 {toyota2.GetHashCode()}");
+//Console.WriteLine($"BMW1 {bmw1.GetHashCode()}");
+//Console.WriteLine($"BMW2 {bmw2.GetHashCode()}");
+
+using Adv10;
+
+CarService cs = new CarService();
+var v12Engine = cs.FindCarsByEngineType();
+cs.ListAll(v12Engine);
+cs.ListAll();
