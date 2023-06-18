a3806b736c020dfd39d697e3a32bd0413b66f4fd
Marsela Shani <marsela.shani@gmail.com>
2023-06-18 13:46:39 +0200

    Adv18

---
+++ Adv18/Program.cs
@@ -0,0 +1,18 @@
+﻿using Adv18;
+
+Zoo zoo = new Zoo();
+zoo.AddAnimals("lion", 2);
+zoo.AddAnimals("duck", 12);
+zoo.AddAnimals("tiger", 3);
+zoo.AddAnimals("wolf", 5);
+zoo.AddAnimals("lion", 1);
+zoo.AddAnimals("duck", 1);
+zoo.AddAnimals("elephant", 1);
+
+var animals = zoo.GetAnimalsCountSorted();
+foreach(var e in animals)
+{
+    Console.WriteLine($"{e.Key}: {e.Value}");
+}
+
+
---
+++ Adv18/Zoo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv18
+{
+    internal class Zoo
+    {
+        public List<string> Animals { get; set; }
+        public Zoo() { 
+            Animals= new List<string>();
+        }
+
+        public int GetNumberOfAllAnimals()
+        {
+            return Animals.Count;
+        }
+        public Dictionary<string, int> GetAnimalsCount()
+        {
+            var result = new Dictionary<string, int>();
+            foreach (var animal in Animals)
+            {
+                if (result.ContainsKey(animal)){
+                    result[animal]++;
+                }
+                else{
+                    result.Add(animal, 1);
+                }
+            }
+            return result;
+        }
+        public Dictionary<string, int> GetAnimalsCountSorted()
+        {
+            var dict = GetAnimalsCount();
+            var result = dict
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+            return result;
+        }
+
+        public void AddAnimals(string animal, int number)
+        {
+            for(int i = 1; i <= number; i++)
+            {
+                Animals.Add(animal);
+            }
+        }
+    }
+}
