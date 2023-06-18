3181aa8b1a1c960964715bebe1e9a4fa7dafbbf0
Marsela Shani <marsela.shani@gmail.com>
2023-06-18 14:55:28 +0200

    Adv20

---
+++ Adv20/Joiner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv20
+{
+    internal class Joiner
+    {  
+        string separator;
+        public Joiner(string separator)
+        {
+            this.separator = separator;
+        }
+        public string Join<T>(List<T> list)
+        {
+            var result = new StringBuilder();
+            int counter = 1;
+            foreach (var item in list)
+            {
+                result.Append(item.ToString());
+                if (counter != list.Count)
+                {
+                    result.Append(separator);
+                }
+                counter++;
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ Adv20/Program.cs
@@ -0,0 +1,19 @@
+﻿using Adv20;
+
+Joiner joiner = new Joiner(", ");
+List<int> ints = new List<int> { 10, 8, 3 };
+Console.WriteLine(joiner.Join(ints));
+
+List<double> doubles = new List<double> { 14.2, 9.3, 3.1 };
+Console.WriteLine(joiner.Join(doubles));
+
+List<string> strings = new List<string> { "loli", "coli", "test" };
+Console.WriteLine(joiner.Join(strings));
+
+List<bool> bools = new List<bool> { false, false, false };
+Console.WriteLine(joiner.Join(bools));
+
+
+
+
+
