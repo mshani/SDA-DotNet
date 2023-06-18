f12ac4ee73186fc059b9a0885091dcaa98eaf8ec
Marsela Shani <marsela.shani@gmail.com>
2023-06-18 15:22:33 +0200

    Adv21

---
+++ Adv21/Extender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv21
+{
+    internal static class Extender
+    {
+        public static List<T> GetEveryNthElement<T>(List<T> list, int index, int skip)
+        {
+            //[a, b, c, d, e, f, g]
+            //index = 2 and skip = 3
+            List<T> result = new List<T>();
+            int current = 0;
+            foreach (T item in list)
+            {
+                if(current == index || current > index + skip)
+                {
+                    result.Add(item);
+                }
+                current++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Adv21/Program.cs
@@ -0,0 +1,15 @@
+﻿using Adv21;
+
+List<char> chars = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+var list = Extender.GetEveryNthElement(chars, 0, 0);
+foreach (char c in list)
+{
+    Console.Write($"{c} ");
+}
+Console.WriteLine();
+List<int> ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var list2 = Extender.GetEveryNthElement(ints, 2, 2);
+foreach (int c in list2)
+{
+    Console.Write($"{c} ");
+}
