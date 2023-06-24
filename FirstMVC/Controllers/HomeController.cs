f291d322bce9aeff728f26f9b2c4bce80efc22d3
Marsela Shani <marsela.shani@gmail.com>
2023-06-24 13:41:50 +0200

    Add model

--- FirstMVC/Controllers/HomeController.cs
+++ FirstMVC/Controllers/HomeController.cs
@@ -15,7 +15,11 @@ namespace FirstMVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var list = new List<Person>();
+            list.Add(new Person { Name = "John", Surname = "Smith" , Age = 25, Gender = 'M'});
+            list.Add(new Person { Name = "Jane", Surname = "Doe", Age = 21, Gender = 'F' });
+            list.Add(new Person { Name = "Anna", Surname = "Johnson", Age = 24, Gender = 'F' });
+            return View(list);
         }
 
         public IActionResult Privacy()
@@ -26,6 +30,16 @@ namespace FirstMVC.Controllers
         public IActionResult About()
         {
             return View();
+        }
+
+        public IActionResult Details(Person person)
+        {
+            return View(person);
+        }
+
+        public IActionResult Edit(Person person)
+        {
+            return View(person);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- FirstMVC/Models/Person.cs
+++ FirstMVC/Models/Person.cs
@@ -4,5 +4,7 @@
     {
         public string Name { get; set; }
         public string Surname { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
     }
 }
